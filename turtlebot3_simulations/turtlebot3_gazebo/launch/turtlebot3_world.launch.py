08fbd74c407ddd639caa7bca25070553ee63dd2e
robertofinnisaro <43074637+robertofinnisaro@users.noreply.github.com>
2025-02-27 09:43:16 +0000

    Add files via upload

---
+++ turtlebot3_simulations/turtlebot3_gazebo/launch/turtlebot3_world.launch.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+#
+# Copyright 2019 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Joep Tool
+
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir,LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory('turtlebot3_gazebo')
+    world_file_name = 'turtlebot3_world.world'
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir, 'models')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    x_pose = LaunchConfiguration('x_pose', default='-2.0')
+    y_pose = LaunchConfiguration('y_pose', default='-0.5')
+
+    world = os.path.join(pkg_dir, 'worlds', world_file_name)
+    
+    gazebo = ExecuteProcess(
+    	cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so', 
+    	'-s', 'libgazebo_ros_factory.so'],
+    	output='screen')
+
+
+    spawn_turtlebot_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose
+        }.items()
+    )
+
+    return LaunchDescription([
+    	gazebo,
+    	spawn_turtlebot_cmd,
+    ])
+    
+
